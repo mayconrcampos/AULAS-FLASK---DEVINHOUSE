95633e3aba2169446b3d0bc1149f73d803f8e671
mayconrcampos <maycon.campos@gmail.com>
2022-07-27 22:30:48 -0300

    quase um crud completo

--- SEMANA 09/Aula 02/meuapp/src/app/controllers/technologies.py
+++ SEMANA 09/Aula 02/meuapp/src/app/controllers/technologies.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Blueprint, request
 
 technology = Blueprint("technology", __name__, url_prefix="/technology")
@@ -11,6 +12,22 @@ def list_all_technologies():
         "data": data
     }
 
+@technology.route("/<int:id_tech>", methods=["GET"])
+def list_one_technology(id_tech):
+
+
+    for _ in range(len(data)):
+        if _ == id_tech:
+            return {
+                "data": data[_]
+            }
+    
+    return {
+        "mensagem": f"ID {id_tech} não encontrado." 
+    }, 404
+
+    
+
 @technology.route("/", methods=["POST"])
 def insert_technologies():
     tech = request.get_json()
@@ -20,3 +37,20 @@ def insert_technologies():
     return {
         "mensagem": "Dado inserido com sucesso"
     }
+
+@technology.route("/", methods=["DELETE"])
+def pop_item():
+    if len(data) > 0:
+        dado = data[-1]
+        data.pop()
+
+        return {
+            "mensagem": f"{dado} excluido com sucesso"
+        }
+    return {
+        "mensagem": "Lista vazia"
+    }, 404
+
+    
+
+    
